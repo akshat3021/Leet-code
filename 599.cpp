4833296fba92f383aa7c7c2ff126e9e982b34967
Akshat Aswal <akshataswal84@gmail.com>
2026-05-21 16:44:30 +0530

    599. Minimum Index Sum of Two Lists

---
+++ 599.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        
+        unordered_map<string, int> mp;
+        for (int i = 0; i < list1.size(); i++) {
+            mp[list1[i]] = i;
+        }
+        vector<string> ans;
+        int miniSum = INT_MAX;
+        for (int j = 0; j < list2.size(); j++) {
+            if (mp.find(list2[j]) != mp.end()) {
+                int sum = j + mp[list2[j]];
+                if (sum < miniSum) {
+                    miniSum = sum;
+                    ans.clear();
+                    ans.push_back(list2[j]);
+                }
+                else if (sum == miniSum) {
+                    ans.push_back(list2[j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
