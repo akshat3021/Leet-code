d64ee1164a45a9e6c04d674f13409a8d9d297537
Akshat Aswal <akshataswal84@gmail.com>
2026-06-25 23:03:25 +0530

    2553. Separate the Digits in an Array

---
+++ 2553.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+         vector<int> ans;
+        for (int i : nums) {
+            vector<int> temp;
+            while (i > 0) {
+                temp.push_back(i % 10);
+                i /= 10;
+            }
+            reverse(temp.begin(), temp.end());
+            for (int j : temp) {
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
+};
